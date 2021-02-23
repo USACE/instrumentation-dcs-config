39472ced426d7058f3eb14cb566f9a2e386ea3b7
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-02-23 09:27:15 -0500

    get ToUnitsAbbrev from XML file

--- _processing/utils/add_uuids.py
+++ _processing/utils/add_uuids.py
@@ -205,10 +205,20 @@ for p in platforms:
         platform_obj['transportmedium'] = tm
 
     config_sensors = p.findall('PlatformConfig/ConfigSensor')
+    script_sensors = p.findall('PlatformConfig/DecodesScript/ScriptSensor')
     cfg_sensors = {}
     if config_sensors is not None:
         print('\n*** ConfigSensor ***')
         for cs in config_sensors:
+            
+            # Get the ScriptSensor for this ConfigSensor
+            # to determine the units
+            sensor_number = cs.get('SensorNumber')
+            
+            for ss in script_sensors:
+                if ss.get('SensorNumber') == sensor_number:       
+                    units = ss.find('UnitConverter').get('ToUnitsAbbr')
+
             sensor_name = cs.find('SensorName').text.strip()
             print(f'SensorName: {sensor_name}')
             dataTypes = cs.findall('DataType')
@@ -217,10 +227,9 @@ for p in platforms:
                 for name, value in dt.attrib.items():
                     print(f'{dt.tag}: {name} -> {value}')
                     dtypes[name] = value 
-            
+                    dtypes['ToUnitsAbbr'] = units            
 
             cfg_sensors[sensor_name] = dtypes
-
     
     platform_obj['config_sensors'] = cfg_sensors
     platform_obj['platform_sensors'] = ps_sites
